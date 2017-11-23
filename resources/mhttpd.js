ad6ace9b33a452ac374cd75b143eb07cd773442b
Stefan Ritt <stefan.ritt@psi.ch>
2017-11-23 12:20:20 +0100

    Fixed promise catch function for mjsonrpc_send_request to properly show ALL errors

--- resources/mhttpd.js
+++ resources/mhttpd.js
@@ -856,7 +856,7 @@ function mhttpd_refresh() {
 
    }).catch(function (error) {
 
-      if (error.xhr.readyState == 4 && error.xhr.status == 0) {
+      if (error.xhr && error.xhr.readyState == 4 && error.xhr.status == 0) {
          mhttpd_error('Connection to server broken. Trying to reconnect&nbsp;&nbsp;');
          document.getElementById("mheader_error").appendChild(mhttpd_spinning_wheel);
          mhttpd_reconnect_id = window.setTimeout(mhttpd_reconnect, 1000);
