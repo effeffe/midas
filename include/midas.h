3a76399e298036d4f5d9c3ed5b27817be4b288aa
Stefan Ritt <stefan.ritt@psi.ch>
2018-01-05 14:10:05 +0100

    Define OS_DARWIN automatically if compiling under MacOSX

--- include/midas.h
+++ include/midas.h
@@ -73,6 +73,13 @@ The main include file
 #endif
 #endif
 
+#if !defined(OS_DARWIN)
+#if defined ( __APPLE__ )
+#define OS_LINUX
+#define OS_DARWIN
+#endif
+#endif
+
 #if defined(OS_LINUX) || defined(OS_OSF1) || defined(OS_ULTRIX) || defined(OS_FREEBSD) || defined(OS_SOLARIS) || defined(OS_IRIX) || defined(OS_DARWIN)
 #define OS_UNIX
 #endif
