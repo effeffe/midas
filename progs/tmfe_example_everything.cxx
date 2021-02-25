5dc61198d8c03a05956b88a3dce1987c9e84db87
Konstantin Olchanski <olchansk@home>
2021-02-25 10:40:18 -0800

    tmfe - add main program and add an "everything" example.

---
+++ progs/tmfe_example_everything.cxx
@@ -0,0 +1,184 @@
+/*******************************************************************\
+
+  Name:         tmfe_example_everything.cxx
+  Created by:   K.Olchanski
+
+  Contents:     Example Front end to demonstrate all functions of TMFE class
+
+\********************************************************************/
+
+#undef NDEBUG // midas required assert() to be always enabled
+
+#include <stdio.h>
+#include <math.h> // M_PI
+
+#include "tmfe.h"
+
+class EqEverything :
+   public TMFeEquipmentBase,
+   public TMFeRpcHandlerInterface,
+   public TMFePeriodicHandlerInterface,
+   public TMFePollHandlerInterface
+{
+public:
+   EqEverything() // ctor
+   {
+      printf("EqEverything::ctor!\n");
+   }
+
+   ~EqEverything() // dtor
+   {
+      printf("EqEverything::dtor!\n");
+   }
+
+   void Usage()
+   {
+      printf("EqEverything::Usage!\n");
+   }
+
+   TMFeResult Init(const std::vector<std::string>& args)
+   {
+      printf("EqEverything::Init!\n");
+      fMfe->RegisterRpcHandler(this);
+      fMfe->RegisterPeriodicHandler(fEq, this);
+      fMfe->RegisterTransitionStartAbort();
+      fEq->SetStatus("Started...", "white");
+      return TMFeOk();
+   }
+
+   TMFeResult HandleRpc(const char* cmd, const char* args, std::string& response)
+   {
+      fMfe->Msg(MINFO, "HandleRpc", "RPC cmd [%s], args [%s]", cmd, args);
+
+      // RPC handler
+      
+      char tmp[256];
+      time_t now = time(NULL);
+      sprintf(tmp, "{ \"current_time\" : [ %d, \"%s\"] }", (int)now, ctime(&now));
+      
+      response = tmp;
+
+      return TMFeOk();
+   }
+
+   TMFeResult HandleBeginRun(int run_number)
+   {
+      fMfe->Msg(MINFO, "HandleBeginRun", "Begin run %d!", run_number);
+      fEq->SetStatus("Running", "#00FF00");
+      return TMFeOk();
+   }
+
+   TMFeResult HandleEndRun(int run_number)
+   {
+      fMfe->Msg(MINFO, "HandleEndRun", "End run %d!", run_number);
+      fEq->SetStatus("Stopped", "#FFFFFF");
+      return TMFeOk();
+   }
+
+   TMFeResult HandlePauseRun(int run_number)
+   {
+      fMfe->Msg(MINFO, "HandlePauseRun", "Pause run %d!", run_number);
+      fEq->SetStatus("Paused", "#FFFF00");
+      return TMFeOk();
+   }
+
+   TMFeResult HandleResumeRun(int run_number)
+   {
+      fMfe->Msg(MINFO, "HandleResumeRun", "Resume run %d!", run_number);
+      fEq->SetStatus("Running", "#00FF00");
+      return TMFeOk();
+   }
+
+   TMFeResult HandleStartAbortRun(int run_number)
+   {
+      fMfe->Msg(MINFO, "HandleStartAbortRun", "Begin run %d aborted!", run_number);
+      fEq->SetStatus("Stopped", "#FFFFFF");
+      return TMFeOk();
+   }
+
+   void SendData(double dvalue)
+   {
+      char buf[1024];
+      fEq->ComposeEvent(buf, sizeof(buf));
+      fEq->BkInit(buf, sizeof(buf));
+         
+      double* ptr = (double*)fEq->BkOpen(buf, "data", TID_DOUBLE);
+      *ptr++ = dvalue;
+      fEq->BkClose(buf, ptr);
+
+      fEq->SendEvent(buf);
+   }
+
+   void HandlePeriodic()
+   {
+      printf("EqEverything::HandlePeriodic!\n");
+      double t = TMFE::GetTime();
+      double data = 100.0*sin(0*M_PI/2.0+M_PI*t/40);
+      SendData(data);
+      fEq->WriteStatistics();
+      char status_buf[256];
+      sprintf(status_buf, "value %.1f", data);
+      fEq->SetStatus(status_buf, "#00FF00");
+   }
+
+   bool HandlePoll()
+   {
+      printf("EqEverything::HandlePoll!\n");
+      return false;
+   }
+
+   void HandleRead()
+   {
+      printf("EqEverything::HandleRead!\n");
+   }
+};
+
+struct EqEverythingCommon: TMFeCommon { EqEverythingCommon() {
+   printf("EqEverythingCommon::ctor!\n");
+   EventID = 1;
+   Period = 1000;
+   LogHistory = 1;
+   WriteEventsToOdb = true;
+} };
+
+static TMFeRegister eq_everything_register("tmfe_example_everything", "test_everything", __FILE__, new EqEverything(), new EqEverythingCommon);
+
+// example frontend hooks
+
+static class EqEverythingHooks: public TMFeHooksInterface
+{
+public:
+   EqEverythingHooks() // ctor
+   {
+      printf("EqEverythingHooks::ctor!\n");
+      TMFE::Instance()->AddHooks(this);
+   }
+
+   void HandlePreConnect(const std::vector<std::string>& args)
+   {
+      printf("EqEverythingHooks::HandlePreConnect!\n");
+   };
+   
+   void HandlePostConnect(const std::vector<std::string>& args)
+   {
+      printf("EqEverythingHooks::HandlePostConnect!\n");
+   };
+   
+   void HandlePreDisconnect()
+   {
+      printf("EqEverythingHooks::HandlePreDisconnect!\n");
+   };
+   
+   void HandlePostDisconnect()
+   {
+      printf("EqEverythingHooks::HandlePostDisconnect!\n");
+   };
+} eq_everything_hooks;
+
+/* emacs
+ * Local Variables:
+ * tab-width: 8
+ * c-basic-offset: 3
+ * indent-tabs-mode: nil
+ * End:
+ */
---
+++ src/tmfe_main.cxx
@@ -0,0 +1,97 @@
+/*******************************************************************\
+
+  Name:         tmfe_main.cxx
+  Created by:   K.Olchanski
+
+  Contents:     Main program for TMFE frontends
+
+\********************************************************************/
+
+#undef NDEBUG // midas required assert() to be always enabled
+
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <unistd.h>
+//#include <stdint.h>
+//#include <sys/time.h>
+//#include <sys/types.h>
+//#include <sys/stat.h>
+//#include <fcntl.h>
+//#include <errno.h>
+//#include <math.h>
+//#include <ctype.h>
+//#include <assert.h>
+//#include <string.h>
+
+#include "tmfe.h"
+
+static void usage(const char* argv0)
+{
+   fprintf(stderr, "Usage: %s ...\n", argv0);
+   exit(1);
+}
+
+int main(int argc, char* argv[])
+{
+   setbuf(stdout, NULL);
+   setbuf(stderr, NULL);
+
+   signal(SIGPIPE, SIG_IGN);
+
+   std::vector<std::string> eq_args;
+
+   //std::string name = "";
+   //
+   //if (argc == 2) {
+   //   name = argv[1];
+   //} else {
+   //   usage(); // DOES NOT RETURN
+   //}
+
+   TMFE* mfe = TMFE::Instance();
+
+   // call pre-connect hook before calling Usage(). Otherwise, if user creates
+   // new equipments inside the hook, they will never see it's Usage(). K.O.
+   mfe->CallPreConnectHooks(eq_args);
+
+   if (0) {
+      usage(argv[0]);
+      mfe->Usage();
+   }
+
+   TMFeResult result = mfe->Connect("tmfe_main", __FILE__);
+   if (result.error_flag) {
+      fprintf(stderr, "Cannot connect to MIDAS, error \"%s\", bye.\n", result.error_message.c_str());
+      return 1;
+   }
+
+   mfe->CallPostConnectHooks(eq_args);
+
+   //mfe->SetWatchdogSec(0);
+   //mfe->SetTransitionSequenceStart(910);
+   //mfe->SetTransitionSequenceStop(90);
+   //mfe->DeregisterTransitionPause();
+   //mfe->DeregisterTransitionResume();
+   //mfe->RegisterTransitionStartAbort();
+
+   mfe->InitEquipments(eq_args);
+
+   while (!mfe->fShutdownRequested) {
+      mfe->PollMidas(10);
+   }
+
+   mfe->CallPreDisconnectHooks();
+   mfe->DeleteEquipments();
+   mfe->Disconnect();
+   mfe->CallPostDisconnectHooks();
+
+   return 0;
+}
+
+/* emacs
+ * Local Variables:
+ * tab-width: 8
+ * c-basic-offset: 3
+ * indent-tabs-mode: nil
+ * End:
+ */
