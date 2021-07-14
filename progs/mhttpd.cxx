4778d87883dd63bf02689a88f51e101b6a438d14
Konstantin Olchanski <olchansk@home>
2021-07-14 12:50:23 -0700

    history - split enable_factor and show_factor

--- progs/mhttpd.cxx
+++ progs/mhttpd.cxx
@@ -10290,7 +10290,8 @@ struct HistPlot
    bool show_run_markers = true;
    bool show_values = true;
    bool show_fill   = true;
-   bool enable_factor = false;
+   bool show_factor = false;
+   bool enable_factor = true;
 
    std::vector<HistVar> vars;
 };
@@ -12061,7 +12062,7 @@ static bool cmp_vars(const HistVar &a, const HistVar &b)
 static void PrintHistPlot(const HistPlot& hp)
 {
    printf("hist plot: %d variables\n", (int)hp.vars.size());
-   printf("timescale: %s, minimum: %f, maximum: %f, zero_ylow: %d, log_axis: %d, show_run_markers: %d, show_values: %d, show_fill: %d, enable_factor: %d\n", hp.timescale.c_str(), hp.minimum, hp.maximum, hp.zero_ylow, hp.log_axis, hp.show_run_markers, hp.show_values, hp.show_fill, hp.enable_factor);
+   printf("timescale: %s, minimum: %f, maximum: %f, zero_ylow: %d, log_axis: %d, show_run_markers: %d, show_values: %d, show_fill: %d, show_factor %d, enable_factor: %d\n", hp.timescale.c_str(), hp.minimum, hp.maximum, hp.zero_ylow, hp.log_axis, hp.show_run_markers, hp.show_values, hp.show_fill, hp.show_factor, hp.enable_factor);
    
    for (size_t i=0; i<hp.vars.size(); i++) {
       printf("var[%d] event [%s][%s] formula [%s], colour [%s] label [%s] factor %f offset %f voffset %f order %d\n", (int)i, hp.vars[i].event_name.c_str(), hp.vars[i].tag_name.c_str(), hp.vars[i].formula.c_str(), hp.vars[i].colour.c_str(), hp.vars[i].label.c_str(), hp.vars[i].factor, hp.vars[i].offset , hp.vars[i].voffset, hp.vars[i].order);
@@ -12124,7 +12125,8 @@ static void LoadHistPlotFromOdb(MVOdb* odb, HistPlot* hp, const char* group, con
    o->RB("Show run markers", &hp->show_run_markers);
    o->RB("Show values", &hp->show_values);
    o->RB("Show fill",   &hp->show_fill);
-   o->RB("Enable factor and offset", &hp->enable_factor);
+   o->RB("Show factor", &hp->show_factor);
+   //o->RB("Enable factor and offset", &hp->enable_factor);
 
    std::vector<std::string> hist_vars;
    std::vector<std::string> hist_formula;
@@ -12194,7 +12196,7 @@ static void LoadHistPlotFromOdb(MVOdb* odb, HistPlot* hp, const char* group, con
       v.order   = NextHistPlotOrder(*hp);
 
       // one-time migration of factor and offset to formula
-      if (v.formula.empty()) {
+      if (hp->enable_factor && v.formula.empty()) {
          if (v.factor!=1 || v.offset!=0 || v.voffset!=0) {
             v.formula = msprintf("%g%+g*(x%+g)", v.offset, v.factor, -v.voffset);
          }
@@ -12219,7 +12221,8 @@ static void LoadHistPlotFromParam(HistPlot* hp, Param* p)
    hp->show_run_markers = *p->getparam("run_markers");
    hp->show_values      = *p->getparam("show_values");
    hp->show_fill        = *p->getparam("show_fill");
-   hp->enable_factor    = *p->getparam("enable_factor");
+   hp->show_factor      = *p->getparam("show_factor");
+   //hp->enable_factor    = *p->getparam("enable_factor");
    
    for (int index=0; ; index++) {
       char str[256];
@@ -12338,7 +12341,8 @@ static void SaveHistPlotToOdb(MVOdb* odb, const HistPlot& hp, const char* group,
    o->WB("Show run markers", hp.show_run_markers);
    o->WB("Show values", hp.show_values);
    o->WB("Show fill", hp.show_fill);
-   o->WB("Enable factor and offset", hp.enable_factor);
+   o->WB("Show factor and offset", hp.show_factor);
+   //o->WB("Enable factor and offset", hp.enable_factor);
 
    std::vector<std::string> hist_vars;
    std::vector<std::string> hist_formula;
@@ -12524,6 +12528,12 @@ void show_hist_config_page(MVOdb* odb, Param* p, Return* r, const char *hgroup, 
    int old_vars = *p->getparam("old_vars");
    r->rsprintf("&nbsp;&nbsp;<input type=checkbox %s name=old_vars value=1 onclick=\"this.form.submit();\">Show deleted and renamed variables", old_vars?"checked":"");
 
+   if (hp.show_factor)
+      r->rsprintf("&nbsp;&nbsp;<input type=checkbox checked name=show_factor value=1 onclick=\"document.form1.hcmd.value='Refresh';document.form1.submit()\">");
+   else
+      r->rsprintf("&nbsp;&nbsp;<input type=checkbox name=show_factor value=1 onclick=\"document.form1.hcmd.value='Refresh';document.form1.submit()\">");
+   r->rsprintf("Show&nbsp;factor&nbsp;and&nbsp;offset\n");
+
    /* hidden command for refresh */
    r->rsprintf("<input type=hidden name=cmd value=Oldhistory>\n");
    r->rsprintf("<input type=hidden name=hcmd value=Refresh>\n");
@@ -12575,12 +12585,6 @@ void show_hist_config_page(MVOdb* odb, Param* p, Return* r, const char *hgroup, 
    else
       r->rsprintf("&nbsp;&nbsp;<input type=checkbox name=show_fill value=1>");
    r->rsprintf("Show&nbsp;graph&nbsp;fill\n");
-
-   if (hp.enable_factor)
-      r->rsprintf("&nbsp;&nbsp;<input type=checkbox checked name=enable_factor value=1 onclick=\"document.form1.hcmd.value='Refresh';document.form1.submit()\">");
-   else
-      r->rsprintf("&nbsp;&nbsp;<input type=checkbox name=enable_factor value=1 onclick=\"document.form1.hcmd.value='Refresh';document.form1.submit()\">");
-   r->rsprintf("Enable&nbsp;factor&nbsp;and&nbsp;offset\n");
 
    r->rsprintf("</td></tr>\n");
 
@@ -12740,14 +12744,13 @@ void show_hist_config_page(MVOdb* odb, Param* p, Return* r, const char *hgroup, 
    }
 
    r->rsprintf("<tr><td colspan=10 style='text-align:left'>Use the \"order\" to reorder or delete history plots. Change order values and press \"refresh\", use 0 or -1 to delete a plot</td></tr>\n");
-   r->rsprintf("<tr><td colspan=10 style='text-align:left'>Use the \"formula\" to transform history data to physics values</td></tr>\n");
-   r->rsprintf("<tr><td colspan=10 style='text-align:left'>Use the \"offset\" and \"factor\" to position the individual graphics on the history plot</td></tr>\n");
-   r->rsprintf("<tr><td colspan=10 style='text-align:left'>displayed_value = offset + factor*(formula(history_value) - voffset)</td></tr>\n");
-   r->rsprintf("<tr><td colspan=10 style='text-align:left'>\"formula\" format is \"3*x+4\" or \"10*sin(x)\". all javascript math functions can be used</td></tr>\n");
+   r->rsprintf("<tr><td colspan=10 style='text-align:left'>Old history: displayed_value = offset + factor*(history_value - voffset)</td></tr>\n");
+   r->rsprintf("<tr><td colspan=10 style='text-align:left'>New history: displayed_value = formula(history_value)</td></tr>\n");
+   r->rsprintf("<tr><td colspan=10 style='text-align:left'>Formula format: \"3*x+4\", \"10*Math.sin(x)\", etc. all javascript math functions can be used</td></tr>\n");
 
    r->rsprintf("<tr>\n");
    r->rsprintf("<th>Col<th>Event<th>Variable<th>Formula<th>Colour<th>Label<th>Order");
-   if (hp.enable_factor) {
+   if (hp.show_factor) {
       r->rsprintf("<th>Factor<th>Offset<th>VOffset");
    }
    r->rsprintf("</tr>\n");
@@ -12882,7 +12885,7 @@ void show_hist_config_page(MVOdb* odb, Param* p, Return* r, const char *hgroup, 
          r->rsprintf("<td><input type=text size=8 maxlength=10 name=\"col%d\" value=%s></td>\n", (int)index, hp.vars[index].colour.c_str());
          r->rsprintf("<td><input type=text size=8 maxlength=%d name=\"lab%d\" value=\"%s\"></td>\n", NAME_LENGTH, (int)index, hp.vars[index].label.c_str());
          r->rsprintf("<td><input type=text size=3 maxlength=32 name=\"ord%d\" value=\"%d\"></td>\n", (int)index, hp.vars[index].order);
-         if (hp.enable_factor) {
+         if (hp.show_factor) {
             r->rsprintf("<td><input type=text size=6 maxlength=32 name=\"factor%d\" value=\"%g\"></td>\n", (int)index, hp.vars[index].factor);
             r->rsprintf("<td><input type=text size=6 maxlength=32 name=\"offset%d\" value=\"%g\"></td>\n", (int)index, hp.vars[index].offset);
             r->rsprintf("<td><input type=text size=6 maxlength=32 name=\"voffset%d\" value=\"%g\"></td>\n", (int)index, hp.vars[index].voffset);
