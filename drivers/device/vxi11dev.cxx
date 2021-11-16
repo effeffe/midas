d1b1b8150973d7687006123821e2720848e13777
ritt <stefan.ritt@psi.ch>
2021-11-16 15:01:36 +0100

    Reformatted and cleaned up code

--- drivers/device/vxi11dev.cxx
+++ drivers/device/vxi11dev.cxx
@@ -17,8 +17,10 @@
 /* #include "dbDefs.h" */
 
 #include "vxi11_user.h"
+
 #undef TRUE   // conflicting type definition between /usr/include/rpc/types.h
 #undef FALSE  // and midas.h
+
 #include "midas.h"
 #include "msystem.h"
 #include "vxi11dev.h"
@@ -29,26 +31,21 @@
 #define TYPE_LENGTH 32     /* length of type string */
 
 typedef struct {
-   CLINK   *clink;
-   char    ip_address[32]; // IP address of the device
-   INT     num_vars;       // Number of all variables
-   char    *cmd_str;       // Command string
-   char    *type_str;      // Type string
+   CLINK *clink;
+   char ip_address[32]; // IP address of the device
+   INT num_vars;       // Number of all variables
+   char *cmd_str;       // Command string
+   char *type_str;      // Type string
 } VXI_INFO;
 
-static CLINK* clink = 0;
-static bool busy = false;
-
-const char* vars_onoff[] = {"OFF", "ON"};
-const char* vars_IV[] = {"CURR", "VOLT"};
-const char* vars_trig[] = {"INT", "INT2", "EXT"};
-const char* vars_func[] = {"SIN", "SQU", "RAMP", "PULS", "NOIS", "DC", "USER"};
+const char *vars_onoff[] = {"OFF", "ON"};
+const char *vars_IV[] = {"CURR", "VOLT"};
+const char *vars_trig[] = {"INT", "INT2", "EXT"};
+const char *vars_func[] = {"SIN", "SQU", "RAMP", "PULS", "NOIS", "DC", "USER"};
 
 /*---- device driver routines --------------------------------------*/
 
-INT vxi11dev_init(HNDLE hKey, void **pinfo, INT nvars)
-{
-
+INT vxi11dev_init(HNDLE hKey, void **pinfo, INT nvars) {
    int status, i, size;
    HNDLE hDB;
    VXI_INFO *info;
@@ -57,59 +54,53 @@ INT vxi11dev_init(HNDLE hKey, void **pinfo, INT nvars)
    cm_get_experiment_database(&hDB, NULL);
 
    /* allocate info structure */
-   info = (VXI_INFO*)calloc(1, sizeof(VXI_INFO));
+   info = (VXI_INFO *) calloc(1, sizeof(VXI_INFO));
    *pinfo = info;
-   
+
    /* IP address */
    size = sizeof(info->ip_address);
    status = db_get_value(hDB, hKey, "IP address", &info->ip_address, &size, TID_STRING, TRUE);
    if (status != DB_SUCCESS)
       return FE_ERR_ODB;
 
-   /* CLINK */
-   info->clink = (CLINK*)calloc(1, sizeof(CLINK));
    cm_msg(MINFO, "vxi11dev_init", "vxi11 initialization with IP: %s", info->ip_address);
    status = FE_SUCCESS;
-   if (!clink) {
-      /* open connection */
-      clink = new CLINK();
-      int ret = vxi11_open_device(info->ip_address, clink);
-      if (ret != 0) {
-         status = FE_ERR_DRIVER;
-	 cm_msg(MERROR, "vxi11dev_init", "vxi11dev driver error: %d", ret);
-      } else {
-	cm_msg(MINFO, "vxi11dev_init", "vxi11dev initialized successfully.");
-      }
+
+   /* open connection */
+   info->clink = new CLINK();
+   int ret = vxi11_open_device(info->ip_address, info->clink);
+   if (ret != 0) {
+      status = FE_ERR_DRIVER;
+      cm_msg(MERROR, "vxi11dev_init", "vxi11dev driver error: %d", ret);
+   } else {
+      cm_msg(MINFO, "vxi11dev_init", "vxi11dev initialized successfully.");
    }
-   info->clink = clink;
 
    /* nvars */
    info->num_vars = nvars;
-   
+
    /* command string */
-   info->cmd_str = (char*)calloc(nvars, CMD_LENGTH);
-   for (i=0; i<nvars; i++)
+   info->cmd_str = (char *) calloc(nvars, CMD_LENGTH);
+   for (i = 0; i < nvars; i++)
       sprintf(info->cmd_str + CMD_LENGTH * i, "<Empty>%d", i);
    db_merge_data(hDB, hKey, "Command", info->cmd_str, CMD_LENGTH * nvars, nvars, TID_STRING);
 
    /* type string */
-   info->type_str = (char*)calloc(nvars, TYPE_LENGTH);
-   for (i=0; i<nvars; i++)
+   info->type_str = (char *) calloc(nvars, TYPE_LENGTH);
+   for (i = 0; i < nvars; i++)
       sprintf(info->type_str + TYPE_LENGTH * i, "<Empty>%d", i);
    db_merge_data(hDB, hKey, "Type", info->type_str, TYPE_LENGTH * nvars, nvars, TID_STRING);
 
    return status;
-   
+
 }
 
 /*----------------------------------------------------------------------------*/
 
-INT vxi11dev_exit(VXI_INFO *info)
-{
+INT vxi11dev_exit(VXI_INFO *info) {
 
-   if (!clink) {
+   if (info->clink) {
       vxi11_close_device(info->ip_address, info->clink);
-      clink = 0;
       free(info->clink);
    }
 
@@ -122,22 +113,21 @@ INT vxi11dev_exit(VXI_INFO *info)
       free(info);
       info = 0;
    }
-   
+
    return FE_SUCCESS;
 }
 
 /*----------------------------------------------------------------------------*/
 
-INT vxi11dev_set(VXI_INFO *info, INT channel, float value)
-{
-   char	cmd[256];
-   char	cmd_base[CMD_LENGTH];
-   int index = (int)value;
-   strlcpy(cmd_base, info->cmd_str + channel*CMD_LENGTH, CMD_LENGTH);
+INT vxi11dev_set(VXI_INFO *info, INT channel, float value) {
+   char cmd[256];
+   char cmd_base[CMD_LENGTH];
+   int index = (int) value;
+   strlcpy(cmd_base, info->cmd_str + channel * CMD_LENGTH, CMD_LENGTH);
 
    // variable type
-   char	type_this[TYPE_LENGTH];
-   strlcpy(type_this, info->type_str + channel*TYPE_LENGTH, TYPE_LENGTH);
+   char type_this[TYPE_LENGTH];
+   strlcpy(type_this, info->type_str + channel * TYPE_LENGTH, TYPE_LENGTH);
 
    // make command
    if (equal_ustring(type_this, "TYPE_NUM")) {
@@ -161,79 +151,66 @@ INT vxi11dev_set(VXI_INFO *info, INT channel, float value)
    }
 
    // send command
-   while (busy) {
-      sleep(3.0);
-   }
-   busy = true;
    long ret = vxi11_send(info->clink, cmd);
-   busy = false;
    if (ret != 0) {
-     cm_msg(MINFO, "vxi11dev_set", "unusual return value in vxi11_send: %ld", ret);
+      cm_msg(MINFO, "vxi11dev_set", "unusual return value in vxi11_send: %ld", ret);
    }
-   
-   busy = false;
-      
+
    return FE_SUCCESS;
 }
 
 /*----------------------------------------------------------------------------*/
 
-INT vxi11dev_get(VXI_INFO *info, INT channel, float *pvalue)
-{
+INT vxi11dev_get(VXI_INFO *info, INT channel, float *pvalue) {
 
    float val;
    int size = sizeof(val);
-   
+
    int i;
-   char	cmd_base[CMD_LENGTH];
-   char	cmd[64];
+   char cmd_base[CMD_LENGTH];
+   char cmd[64];
    char buf[256];
-   strlcpy(cmd_base, info->cmd_str + channel*CMD_LENGTH, CMD_LENGTH);
+   strlcpy(cmd_base, info->cmd_str + channel * CMD_LENGTH, CMD_LENGTH);
    sprintf(cmd, "%s?", cmd_base);
 
    // send and receive from device
-   while (busy) {
-      sleep(3.0);
-   }
-   busy = true;
-   long ret = vxi11_send_and_receive(info->clink, cmd, buf, 256, VXI11_READ_TIMEOUT*10);
-   busy = false;
+   long ret = vxi11_send_and_receive(info->clink, cmd, buf, 256, VXI11_READ_TIMEOUT * 10);
    if (ret != 0) {
-     cm_msg(MINFO, "vxi11dev_get", "unusual return value in vxi11_send_and_receive: %ld", ret);
+      cm_msg(MINFO, "vxi11dev_get", "unusual return value in vxi11_send_and_receive: %ld", ret);
    }
    std::string buf_str = buf;
-   int index =  buf_str.find('\n');
+   int index = buf_str.find('\n');
    if (index >= 0) {
       buf[index] = '\0';
    }
 
    // variable type
-   char	type_this[TYPE_LENGTH];
-   strlcpy(type_this, info->type_str + channel*TYPE_LENGTH, TYPE_LENGTH);
+   char type_this[TYPE_LENGTH];
+   strlcpy(type_this, info->type_str + channel * TYPE_LENGTH, TYPE_LENGTH);
 
    // convert to number
    if (equal_ustring(type_this, "TYPE_NUM")) {
       (*pvalue) = atof(buf);
    } else if (equal_ustring(type_this, "TYPE_ONOFF")) {
-      for (i=0; i<2; i++) {
+      for (i = 0; i < 2; i++) {
          if (equal_ustring(buf, vars_onoff[i])) {
             (*pvalue) = i;
          }
       }
    } else if (equal_ustring(type_this, "TYPE_IV")) {
-      for (i=0; i<2; i++) {
+      for (i = 0; i < 2; i++) {
          if (equal_ustring(buf, vars_IV[i])) {
             (*pvalue) = i;
          }
       }
    } else if (equal_ustring(type_this, "TYPE_TRIG")) {
-      for (i=0; i<3; i++) {
+      for (i = 0; i < 3; i++) {
          if (equal_ustring(buf, vars_trig[i])) {
             (*pvalue) = i;
          }
       }
    } else if (equal_ustring(type_this, "TYPE_FUNC")) {
-      for (i=0; i<7; i++) {
+      for (i = 0; i < 7; i++) {
          if (equal_ustring(buf, vars_func[i])) {
             (*pvalue) = i;
          }
@@ -245,84 +222,79 @@ INT vxi11dev_get(VXI_INFO *info, INT channel, float *pvalue)
 
 /*----------------------------------------------------------------------------*/
 
-INT vxi11dev_get_demand(VXI_INFO *info, INT channel, float *pvalue)
-{
+INT vxi11dev_get_demand(VXI_INFO *info, INT channel, float *pvalue) {
    return FE_SUCCESS;
 }
 
 /*---- device driver entry point -----------------------------------*/
 
-INT vxi11dev(INT cmd, ...)
-{
+INT vxi11dev(INT cmd, ...) {
    va_list argptr;
    HNDLE hKey;
    INT channel, status;
    float value, *pvalue;
    VXI_INFO *info;
-   char *name;
-   
+
    va_start(argptr, cmd);
    status = FE_SUCCESS;
-   
-   if (cmd == CMD_INIT) {
-      void *pinfo;
-      
-      hKey = va_arg(argptr, HNDLE);
-      pinfo = va_arg(argptr, void *);
-      channel = va_arg(argptr, INT);
-      status = vxi11dev_init(hKey, (void**)pinfo, channel);
-   } else {
-      info = va_arg(argptr, VXI_INFO *);
-      
-      /* only execute command if enabled */
-      switch (cmd) {
-         case CMD_INIT:
-            break;
-            
-         case CMD_EXIT:
-            status = vxi11dev_exit(info);
-            break;
-            
-         case CMD_START:
-            break;
-            
-         case CMD_SET:
-            channel = va_arg(argptr, INT);
-            value = (float) va_arg(argptr, double);
-            status = vxi11dev_set(info, channel, value);
-            break;
-            
-         case CMD_GET:
-            channel = va_arg(argptr, INT);
-            pvalue = va_arg(argptr, float *);
-            status = vxi11dev_get(info, channel, pvalue);
-            break;
-            
-         case CMD_GET_DEMAND:
-            channel = va_arg(argptr, INT);
-            pvalue = va_arg(argptr, float *);
-            status = vxi11dev_get_demand(info, channel, pvalue);
-            break;
 
-         case CMD_GET_LABEL:
-            channel = va_arg(argptr, INT);
-            name = va_arg(argptr, char *);
-            strlcpy(name, info->cmd_str + channel*CMD_LENGTH, CMD_LENGTH);
-            break;
+   switch (cmd) {
+      case CMD_INIT:
+         hKey = va_arg(argptr, HNDLE);
+         info = va_arg(argptr, VXI_INFO *);
+         channel = va_arg(argptr, INT);
+         status = vxi11dev_init(hKey, (void **) info, channel);
+         break;
 
-         case CMD_GET_THRESHOLD:
-            channel = va_arg(argptr, INT);
-            pvalue = va_arg(argptr, float *);
-            *pvalue = 0.01;
-            break;
-            
-         default:
-            break;
-      }
+      case CMD_EXIT:
+         info = va_arg(argptr, VXI_INFO *);
+         status = vxi11dev_exit(info);
+         break;
+
+      case CMD_START:
+         break;
+
+      case CMD_SET:
+         info = va_arg(argptr, VXI_INFO *);
+         channel = va_arg(argptr, INT);
+         value = (float) va_arg(argptr, double);
+         status = vxi11dev_set(info, channel, value);
+         break;
+
+      case CMD_GET:
+         info = va_arg(argptr, VXI_INFO *);
+         channel = va_arg(argptr, INT);
+         pvalue = va_arg(argptr, float *);
+         status = vxi11dev_get(info, channel, pvalue);
+         break;
+
+      case CMD_GET_DEMAND:
+         info = va_arg(argptr, VXI_INFO *);
+         channel = va_arg(argptr, INT);
+         pvalue = va_arg(argptr, float *);
+         status = vxi11dev_get_demand(info, channel, pvalue);
+         break;
+
+      case CMD_GET_LABEL:
+         char *name;
+         info = va_arg(argptr, VXI_INFO *);
+         channel = va_arg(argptr, INT);
+         name = va_arg(argptr, char *);
+         strlcpy(name, info->cmd_str + channel * CMD_LENGTH, CMD_LENGTH);
+         break;
+
+      case CMD_GET_THRESHOLD:
+         channel = va_arg(argptr, INT);
+         pvalue = va_arg(argptr, float *);
+         *pvalue = 0.01;
+         break;
+
+      default:
+         break;
    }
-   
+
    va_end(argptr);
-   
+
    return status;
 }
 
