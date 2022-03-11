e948f76d1f977e788f964f3bb400e477de49b344
ritt <stefan.ritt@psi.ch>
2022-03-11 10:33:37 +0100

    Improved debugging output of sequencer and fixed error display

--- progs/msequencer.cxx
+++ progs/msequencer.cxx
@@ -1374,10 +1374,14 @@ void sequencer()
             db_get_record1(hDB, hKeySeq, &seq, &size, 0, strcomb1(sequencer_str).c_str()); // could have changed seq tree
             seq.current_line_number++;
          } else if(status == DB_NO_KEY){
-            seq_error(seq, "Could not match any key!");
+            char str[1024];
+            sprintf(str, "ODB key \"%s\" not found", odbpath);
+            seq_error(seq, str);
          } else {
             //something went really wrong
-            seq_error(seq, "Internal error setting key!");
+            char str[1024];
+            sprintf(str, "Internal error %d", status);
+            seq_error(seq, str);
             return;
          }
 
