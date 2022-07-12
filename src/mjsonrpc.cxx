d8cfbb51d88de83fc4eeeaa55982b9915d26a07e
Stefan Ritt <stefan.ritt@psi.ch>
2022-07-12 20:19:08 +0200

    Finished renaming of links

--- src/mjsonrpc.cxx
+++ src/mjsonrpc.cxx
@@ -1356,7 +1356,7 @@ static MJsonNode* js_db_rename(const MJsonNode* params)
       HNDLE hkey;
       std::string path = (*paths)[i]->GetString();
 
-      status = db_find_key(hDB, 0, path.c_str(), &hkey);
+      status = db_find_link(hDB, 0, path.c_str(), &hkey);
       if (status != DB_SUCCESS) {
          sresult->AddToArray(MJsonNode::MakeInt(status));
          continue;
