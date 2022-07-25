50f771aa3a1e17a6b0ae44ea870b845b068084fb
ritt <stefan.ritt@psi.ch>
2022-07-25 13:24:46 +0200

    Added <nobr> which is robust agains hyphens

--- progs/mhttpd.cxx
+++ progs/mhttpd.cxx
@@ -2806,14 +2806,10 @@ void show_eqtable_page(Param* pp, Return* r, int refresh)
          if (strlen(name) < 1)
             sprintf(name, "[%d]", i);
 
-         std::string n(name);
-         while (n.find(" ") != std::string::npos)
-            n.replace(n.find(" "), 1, "&nbsp;");
-
          if (i % 2 == 0)
-            r->rsprintf("<tr class=\"ODBtableEven\"><td colspan=%d>%s", colspan, n.c_str());
+            r->rsprintf("<tr class=\"ODBtableEven\"><td colspan=%d><nobr>%s</nobr>", colspan, name);
          else
-            r->rsprintf("<tr class=\"ODBtableOdd\"><td colspan=%d>%s", colspan, n.c_str());
+            r->rsprintf("<tr class=\"ODBtableOdd\"><td colspan=%d><nobr>%s</nobr>", colspan, name);
 
          for (int j = 0;; j++) {
             HNDLE hkey;
